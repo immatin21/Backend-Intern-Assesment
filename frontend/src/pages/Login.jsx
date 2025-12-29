48a6f72141771cea4d69b67570ed1dc01914e0fb
immatin21 <mondalmatin04@gmail.com>
2025-12-29 22:05:32 +0530

    login page and auth flow

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import api from "../services/api";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await api.post("/auth/login", { email, password });
+    login(res.data.data.user, res.data.data.token);
+
+    res.data.data.user.role === "admin"
+      ? navigate("/admin")
+      : navigate("/profile");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      <button>Login</button>
+    </form>
+  );
+};
+
+export default Login;
