014846b30d18e7cc61387ebd310bedc27cc0e0b8
immatin21 <mondalmatin04@gmail.com>
2025-12-30 16:16:58 +0530

    Cors issue resolve

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,15 @@ import userRoutes from "./routes/user.routes.js";
 const app = express();
 
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
